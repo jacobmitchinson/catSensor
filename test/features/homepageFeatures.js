107b3cc02c952bf68da86f311d6143651fc7979e
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-02-06 10:50:56 +0000

    Added new feature test to display a cat.

---
+++ test/features/homepageFeatures.js
@@ -0,0 +1,17 @@
+describe('homepage', function() {
+
+  it('should display a cat if the user enters a handle that contains a tweet with profanity', function() {
+    casper.start('http://localhost:9990/')
+    casper.waitForSelector('form', function(){
+
+      this.fill('form#enter-twitter-handle', {
+        'handle' : 'jakobtek'
+      }, true);
+
+    });
+    casper.then(function(){
+      expect('body').to.have.class('image');
+    });
+  });
+
+});
